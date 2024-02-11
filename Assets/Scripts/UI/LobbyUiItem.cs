e4e5b3a8af3e2c4fc59d97b32038b7dc8f71e0f9
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-02-12 05:33:06 +1100

    Made somewhat nice little lobby selection screen

---
+++ Assets/Scripts/UI/LobbyUiItem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUiItem : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI m_lobbyOnlineTypeText;
+    [SerializeField] private TextMeshProUGUI m_lobbyDescription;
+    [SerializeField] private TextMeshProUGUI m_lobbyPlayerCount;
+    [SerializeField] private Button m_joinLobbyButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
