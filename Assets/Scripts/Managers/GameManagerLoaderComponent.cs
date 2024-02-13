aeb3d33f492759c3ce0d7c5736cf7f49c58ad244
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-02-13 19:25:27 +1100

    Backup

---
+++ Assets/Scripts/Managers/GameManagerLoaderComponent.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GameManagerLoaderComponent : MonoBehaviour
+{
+    [SerializeField] private GameObject m_eventSystem;
+    [SerializeField] private GameObject m_gameManager;
+    [SerializeField] private GameObject m_networkManager;
+    
+    private void Awake()
+    {
+        //Network manager cannot be nested so we use that as the manager parent object
+        GameObject networkManager = Instantiate(m_networkManager);
+        networkManager.transform.position = Vector3.zero;
+        networkManager.transform.rotation = Quaternion.Euler(Vector3.zero);
+        networkManager.transform.localScale = Vector3.one;
+        
+        Instantiate(m_eventSystem, networkManager.transform);
+        Instantiate(m_gameManager, networkManager.transform);
+        
+        //Since the other managers are parented under network manager only network needs to be put into the dont destroy on load.
+        DontDestroyOnLoad(networkManager);
+    }
+}
