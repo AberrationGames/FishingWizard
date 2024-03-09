7c53df8c6c3e29ba6870305167f16e324ffbc283
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-03-09 19:40:34 +1100

    Made small change to git pull script, added testing cam to main menu.

--- Assets/Scripts/Managers/GameManagerLoaderComponent.cs
+++ Assets/Scripts/Managers/GameManagerLoaderComponent.cs
@@ -17,17 +17,17 @@ public class GameManagerLoaderComponent : MonoBehaviour
         //    return;
         
         //Network manager cannot be nested so we use that as the manager parent object
-        GameObject networkManager = Instantiate(m_networkManager);
-        networkManager.transform.position = Vector3.zero;
-        networkManager.transform.rotation = Quaternion.Euler(Vector3.zero);
-        networkManager.transform.localScale = Vector3.one;
-        
-        Instantiate(m_eventSystem, networkManager.transform);
-        Instantiate(m_gameManager, networkManager.transform);
-        
-        //Since the other managers are parented under network manager only network needs to be put into the dont destroy on load.
-        DontDestroyOnLoad(networkManager);
-        m_didSpawnObjects = true;
+        //GameObject networkManager = Instantiate(m_networkManager);
+        //networkManager.transform.position = Vector3.zero;
+        //networkManager.transform.rotation = Quaternion.Euler(Vector3.zero);
+        //networkManager.transform.localScale = Vector3.one;
+        //
+        //Instantiate(m_eventSystem, networkManager.transform);
+        //Instantiate(m_gameManager, networkManager.transform);
+        //
+        ////Since the other managers are parented under network manager only network needs to be put into the dont destroy on load.
+        //DontDestroyOnLoad(networkManager);
+        //m_didSpawnObjects = true;
     }
 
     private void Start()
